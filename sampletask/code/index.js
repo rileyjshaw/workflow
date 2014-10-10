d01eaa02893301fdb23a17436d1fd6247a5fce19
Riley Shaw <rileyjshaw@gmail.com>
2014-10-10 08:35:30 -0700

    Add /activeDrivers and /locations routes

--- sampletask/code/index.js
+++ sampletask/code/index.js
@@ -3,12 +3,22 @@ var faker = require('faker');
 var app = express();
 
 var users = [];
-for (var i=0; i<3003; i++) {
+for (var i = 0; i < 3003; i++) {
 	users.push(faker.name.findName());
 }
 
+app.get('/activeDrivers', , function(req, res){
+    // ...
+    res.send('response');
+});
+
 app.get('/users', function(req, res){
     res.send(users);
+});
+
+app.get('/locations', , function(req, res){
+    // ...
+    res.send('response');
 });
 
 var port = process.env.port || 49001;
