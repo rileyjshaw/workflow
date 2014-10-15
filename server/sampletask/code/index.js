7298b3656c49841dd1b4fe619abd50b4b3418f7d
Riley Shaw <rileyjshaw@gmail.com>
2014-10-14 20:16:35 -0700

    Add /maxims and /locations endpoints

--- server/sampletask/code/index.js
+++ server/sampletask/code/index.js
@@ -2,14 +2,37 @@ var express = require('express');
 var faker = require('faker');
 var app = express();
 
-var users = [];
-for (var i=0; i<3003; i++) {
-	users.push(faker.name.findName());
+var citizens = [];
+var locations = {};
+var maxims = {
+	war: 'peace',
+	freedom: 'slavery',
+	ignorance: 'strength'
+};
+
+for (var i = 0; i < 3003; i++) {
+	citizens.push(faker.name.findName());
 }
 
-app.get('/users', function(req, res){
-    res.send(users);
+citizens.forEach(function (citizen) {
+	locations[citizen] = {
+		lat: faker.address.latitude(),
+		lon: faker.address.longitude()
+	};
 });
+
+app.get('/citizens', function(req, res){
+    res.send(citizens);
+});
+
+app.get('/locations', function(req, res){
+    res.send(locations);
+});
+
+app.get('/maxims', function(req, res){
+    res.send(maxims);
+});
+
 
 var port = process.env.port || 49001;
 app.listen(port);
